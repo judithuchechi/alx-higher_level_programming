6585dfda759e56204fa01610e873b89b98033883
judith <judithuchechi62@gmail.com>
2023-01-22 18:32:30 -0800

    correct

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 for i in range(0, 9):
-	for j in range(1, 10):
-		if i == j or i > j:
-			continue
-		if i == 8 and j == 9:
-			print(f"{i}{j}")
-			break
-		print(f"{i}{j}, ", end= '')
+    for j in range(1, 10):
+        if i == j or i > j:
+            continue
+        if i == 8 and j == 9:
+            print("{}{}".format(i, j))
+            break
+        print("{}{}, ".format(i, j), end='')
--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def islower(c):
-	c = ord(c)
-	if c >= 65 and c <= 90:
-		return False
-	elif c >= 97 and c <= 122:
-		return True
+    c = ord(c)
+    if c >= 65 and c <= 90:
+        return False
+    elif c >= 97 and c <= 122:
+        return True
