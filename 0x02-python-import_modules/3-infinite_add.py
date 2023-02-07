550232eec3c8d4c19b329a3b4aa27aacf0ae4b7a
judith <judithuchechi62@gmail.com>
2023-02-07 05:08:31 -0800

    imports

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from sys import argv
+
+    n = len(argv)
+    sum = 0
+
+    for i in range(1, n):
+        sum = sum + (int(argv[i]))
+    print("{}".format(sum))
---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import hidden_4
+
+    lists = dir(hidden_4)
+
+    for i in lists:
+        if i[0] != "_":
+            print("{}".format(i))
