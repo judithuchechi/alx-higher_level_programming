fafc8dd6a03a12a044527c3f0facfaa744b5681e
judith <judithuchechi62@gmail.com>
2023-02-10 17:38:53 -0800

    newcor

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -17,6 +17,12 @@ def add_tuple(tuple_a=(), tuple_b=()):
         if tuple_a == () and tuple_b == ():
             tuple_a = (0, 0)
             tuple_b = (0, 0)
+        if tuple_a == () and len(tuple_b) == 2:
+            tuple_a = (0, 0)
+            tuple_b = (tuple_b[0], tuple_b[1]
+        if len(tuple_a) == 2 and tuple_b == ():
+            tuple_a = (tuple_a[0], tuple_a[1])
+            tuple_b = (0, 0)
 
         add1 = tuple_a[0] + tuple_b[0]
         add2 = tuple_a[1] + tuple_b[1]
