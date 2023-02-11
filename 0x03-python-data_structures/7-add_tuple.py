da2e23951e3d7fe706013488d473ae1c026e3901
judith <judithuchechi62@gmail.com>
2023-02-11 00:01:08 -0800

    correctio

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -4,27 +4,22 @@ def add_tuple(tuple_a=(), tuple_b=()):
 
     new_tup = ()
 
-    for i in range(2):
-        if len(tuple_a) == 1 and len(tuple_b) == 1:
-            tuple_a = (tuple_a[0], 0)
-            tuple_b = (tuple_b[0], 0)
-        if len(tuple_a) == 1 and len(tuple_b) == 2:
-            tuple_a = (tuple_a[0], 0)
-            tuple_b = (tuple_b[0], tuple_b[1])
-        if len(tuple_a) == 2 and len(tuple_b) == 1:
-            tuple_a = (tuple_a[0], tuple_a[1])
-            tuple_b = (tuple_b[0], 0)
-        if tuple_a == () and tuple_b == ():
-            tuple_a = (0, 0)
-            tuple_b = (0, 0)
-        if tuple_a == () and len(tuple_b) == 2:
-            tuple_a = (0, 0)
-            tuple_b = (tuple_b[0], tuple_b[1]
-        if len(tuple_a) == 2 and tuple_b == ():
-            tuple_a = (tuple_a[0], tuple_a[1])
-            tuple_b = (0, 0)
+    if len(tuple_a) == 2:
+        tuple_a = (tuple_a[0], tuple_a[1])
+    if len(tuple_a) == 1:
+        tuple_a = (tuple_a[0], 0)
 
-        add1 = tuple_a[0] + tuple_b[0]
-        add2 = tuple_a[1] + tuple_b[1]
-        new_tup = (add1, add2)
+    if len(tuple_b) == 2:
+        tuple_b = (tuple_b[0], tuple_b[1])
+    if len(tuple_b) == 1:
+        tuple_b = (tuple_b[0], 0)
+
+    if tuple_a == ():
+        tuple_a = (0, 0)
+    if tuple_b == ():
+        tuple_b = (0, 0)
+
+    add1 = tuple_a[0] + tuple_b[0]
+    add2 = tuple_a[1] + tuple_b[1]
+    new_tup = (add1, add2)
     return(new_tup)
