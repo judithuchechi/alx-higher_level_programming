70e2e8a31467a400463ef0c1100249b352dd1afa
judithuchechi <judithuchechi62@gmail.com>
2024-01-18 04:15:56 -0800

    Advanced

---
+++ 0x04-python-more_data_structures/103-python.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "Python.h"
+
+/**
+ *print_python_list_info - prints basic info about Python lists
+ *@p: A PyObject list
+ */
+void print_python_list_info(PyObject *p)
+{
+	int alloc, size, i;
+	PyObject *obj;
+
+	size = Py_SIZE(p);
+	alloc = ((PyListObject *)p)->allocated;
+
+	printf("[*] Size of the Python List = %d\n", size);
+	printf("[*] Allocated = %d\n", alloc);
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Element %d: ", i);
+
+		obj = PyList_GetItem(p, i);
+		printf("%s\n", Py_TYPE(obj)->tp_name);
+	}
+}
+
+/**
+ *print_python_bytes - prints basic info about Python bytes objects
+ *@p: A PyObject list
+ */
+void print_python_bytes(PyObject *p)
+{
+
