f154e3b47be2b9527c38b4d1c55e8700fd403517
root <judithuchechi62@gmail.comgit config --global user.name judithuchechigit config --global user.email judithuchechi62@gmail.com>
2024-02-27 01:38:25 -0800

    new addition

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-    for items in range(x):
+    for items in range(count, x):
         try:
             print("{:d}".format(my_list[items]), end="")
             count += 1
         except (TypeError, ValueError, IndexError):
             pass
+            
     print()
     return count
