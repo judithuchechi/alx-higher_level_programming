d8fbdcd87e04f54452178a4c54890e1e699ecbe8
root <judithuchechi62@gmail.comgit config --global user.name judithuchechigit config --global user.email judithuchechi62@gmail.com>
2024-03-09 06:29:56 -0800

    adavanced

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import sys
+
+
+def safe_function(fct, *args):
+    """
+    Execute a function safely, handling exceptions.
+
+    Parameters:
+    - fct: The function to be executed.
+    - *args: Variable number of arguments to pass to the function.
+
+    Returns:
+    - The result of the function execution if successful, or an error message if an exception occurs.
+    """
+    try:
+        result = fct(*args)
+        return result
+    except Exception as error:
+            print(f"Exception: {error}", file=sys.stderr)
