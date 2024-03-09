2e07feabe661d4ac697035e3ad93b5c389d826c5
root <judithuchechi62@gmail.comgit config --global user.name judithuchechigit config --global user.email judithuchechi62@gmail.com>
2024-03-09 08:12:55 -0800

    new2

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -9,4 +9,4 @@ def safe_function(fct, *args):
         result = fct(*args)
         return result
     except Exception as error:
-            print("Exception: {}".format(error), file=sys.stderr)
+        print("Exception: {}".format(error), file=sys.stderr)
