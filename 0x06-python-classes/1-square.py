11ddea1d9dc842aac28ce3dcfcebaca4235459c8
judithuchechi <judithuchechi62@gmail.com>
2024-04-04 15:20:37 -0700

    add4

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+'''
+1-square.py: Python script that defines square,
+   private instantiation attribute of size
+'''
+
 
 class Square:
+    ''' creates Square class '''
+
     def __init__(self, size):
+        ''' initializes Suare witb size '''
         self.__size = size
