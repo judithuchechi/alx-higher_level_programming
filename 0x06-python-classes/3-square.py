99c8125b990bf712ceb5390162bb53050ddfc43d
judithuchechi <judithuchechi62@gmail.com>
2024-04-06 17:14:21 -0700

    Newadd3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+'''2-square.py: Defines size as an int and also >= 0'''
+
+
+class Square:
+    ''' creates Square type '''
+    def __init__(self, size=0):
+        """
+        Initialize a square with a given size.
+
+        Parameters:
+        - size (int): The size of the square (default is 0).
+
+        Raises:
+        - TypeError: If size is not an integer.
+        - ValueError: If size is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        ''' defines the area of a square'''
+
+        return self.__size * self.__size
